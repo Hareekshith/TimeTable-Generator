a78028487a8de424b33b969d87d193e449109459
Hareekshith <hareekshith@gmail.com>
2025-06-15 14:45:33 +0530

    Progressing forms using useState!

--- frontend/src/pages/details.jsx
+++ frontend/src/pages/details.jsx
@@ -1,19 +1,23 @@
-import { React } from "react";
+import { React , useState } from "react";
 import "./Home.css";
 
 function Details() {
+  const [teacher, setTeacher] = useState({
+    name: "",
+    subject: ""
+  });
+  const [cla, setCla] = useState("");
   return(
     <div className="hero-bg">
-      <div className="home-card">
+    <div className="home-card">
     <form>
-      <label htmlfor="Class 1">Class 1</label>
+    <label htmlfor="Class 1">Class 1</label>
     <input type="text" placeholder="Enter Class name " name="Class" />
-
     <label htmlfor="Class 2">Class 2</label>
     <input type="text" placeholder="Enter Class name " name="Class" />
     </form>
     <button>Submit</button>
-      </div>
+    </div>
     </div>
   );
 }
