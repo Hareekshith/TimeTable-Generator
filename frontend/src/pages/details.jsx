5d912235c5925fcfee6d6a5ec2daab4880dbdce3
Hareekshith <hareekshith@gmail.com>
2025-07-01 12:16:49 +0530

    Mini forms structure remaining!

--- frontend/src/pages/details.jsx
+++ frontend/src/pages/details.jsx
@@ -9,17 +9,22 @@ function Details() {
   const [cla, setCla] = useState("");
   return(
     <div className="hero-bg">
-    <div className="home-card">
-    <form>
-    <label htmlfor="Class 1">Class 1</label>
-    <input type="text" placeholder="Enter Class name " name="Class" />
-    <label htmlfor="Class 2">Class 2</label>
-    <input type="text" placeholder="Enter Class name " name="Class" />
-    </form>
-    <button>Submit</button>
-    </div>
+			<div className="home-card">
+				<form>
+					<h1>For Adding teachers!</h1>
+					<label htmlFor="teacher name">Teacher name</label>
+    			<input type="text" placeholder="Enter name " name="Teacher" />
+					<label htmlFor="subjects">Enter subjects (seperated by commas)</label>
+					<input type="text" placeholder="Enter subjects " name="Subj" />
+					<h1>For adding classes</h1>
+					<label htmlFor="Class name">Class name</label>
+					<input type="text" placeholder="Enter class name " name="Class" />
+					<label htmlFor="sub-teach">Enter subject with teacher name eg. Subject(Teacher name)</label>
+					<input type="text" placeholder="Enter your choices " name="classsubj" />
+				</form>
+				<button>Submit</button>
+			</div>
     </div>
   );
 }
-
 export default Details
