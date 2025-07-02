37b9aec57f754348617eeb3eeb29cf102e30f1e6
Hareekshith <hareekshith@gmail.com>
2025-07-02 23:02:31 +0530

    A spark of an idea!

--- frontend/src/pages/details.jsx
+++ frontend/src/pages/details.jsx
@@ -9,21 +9,25 @@ function Details() {
   const [cla, setCla] = useState("");
   return(
     <div className="hero-bg">
-			<div className="home-card">
+			<div className="home-card" id="one">
 				<form>
 					<h1>For Adding teachers!</h1>
 					<label htmlFor="teacher name">Teacher name</label>
     			<input type="text" placeholder="Enter name " name="Teacher" />
 					<label htmlFor="subjects">Enter subjects (seperated by commas)</label>
 					<input type="text" placeholder="Enter subjects " name="Subj" />
+				</form>
+			</div>
+			<div className="home-card" id="two">
+				<form>
 					<h1>For adding classes</h1>
 					<label htmlFor="Class name">Class name</label>
 					<input type="text" placeholder="Enter class name " name="Class" />
 					<label htmlFor="sub-teach">Enter subject with teacher name eg. Subject(Teacher name)</label>
 					<input type="text" placeholder="Enter your choices " name="classsubj" />
 				</form>
-				<button>Submit</button>
 			</div>
+			<button>Submit</button>
     </div>
   );
 }
