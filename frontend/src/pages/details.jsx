e3039937cb9663065693b061780b682e4d6f5c5b
hareekshith <hareekshith@gmail.com>
2025-07-08 22:00:16 +0530

    Improved font

--- frontend/src/pages/details.jsx
+++ frontend/src/pages/details.jsx
@@ -34,7 +34,7 @@ function Details() {
     <div className="hero-bg">
 			<div className="home-card">
 				<form onSubmit={handleAddTeacher}>
-					<h1>For Adding teachers!</h1>
+					<h1>For Adding Teachers</h1>
 					<label htmlFor="teacher name">Teacher name</label>
   				<input type="text" value={teacher.name} placeholder="Enter name" onChange={(e) => setTeacher({ ...teacher, name: e.target.value })} />
   				<label htmlFor="subjects">Enter subjects (separated by commas)</label>
@@ -52,7 +52,7 @@ function Details() {
 			</div>
 			<div className="home-card">
 				<form onSubmit={handleAddClass}>
-					<h1>For adding classes</h1>
+					<h1>For Adding Classes</h1>
 					<label htmlFor="Class name">Class name</label>
 					<input type="text" placeholder="Enter class name " value={cla.name} onChange={(e) => setCla({...cla, name: e.target.value})} />
 					<label htmlFor="sub-teach">Enter subject with teacher name eg. Subject(Teacher name)</label>
