7d5b2a0211a5bf64074d9aeb03095588adf684e2
hareekshith <hareekshith@gmail.com>
2025-08-12 23:12:56 +0530

    Fixed navigation from login page

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -9,6 +9,7 @@ export default function Login(){
   const [psswd, setPsswd] = useState("");
   const [loggedIn, setLoggedIn] = useState(false);
   const [error, setError] = useState("");
+  const navigate = useNavigate();
 
   async function handleSubmit(e) {
     e.preventDefault();
@@ -19,6 +20,7 @@ export default function Login(){
       } else {
         await signInWithEmailAndPassword(auth, un, psswd);
       }
+      navigate('/details');
     } catch(err){
       setError("Login Failed: "+err.message);
       alert(err.message);
@@ -28,7 +30,7 @@ export default function Login(){
     <div className="hero-bg">
       <div className="login">
         {isNew ? <h1>Introduce yourself!</h1> : <h1>Let's see who you really are!</h1>}
-        <form onSubmit={handleSubmit} style={{gap: "50px", justifyContent: "center", alignItems: 'center', padding: "40px",}}>
+        <form onSubmit={handleSubmit}>
           <input type="email" name="username" value={un} placeholder="Enter email" onChange={e => setUn(e.target.value)} required />
           <input type="password" name="password" value={psswd} placeholder="Enter the password" onChange={e => setPsswd(e.target.value)} required />
           {isNew ? (
@@ -45,7 +47,7 @@ export default function Login(){
           )}
           <button type="submit">{isNew ? "Register" : "Login"}</button>
         </form>
-        {error && <p style={{color:"red"}}>{error}</p>}
+        {error && <p style={{color:"red", gridColumn: "1/-1"}}>{error}</p>}
       </div>
     </div>
   );
